934fedfd0c632170fbec644bd537474cfaf97778
DINA MINGAZOVA <91434740+midinka@users.noreply.github.com>
2023-07-10 08:53:01 +0300

    Create Alpha.cs

---
+++ Alpha.cs
@@ -0,0 +1,16 @@
+
+namespace HelloWorld
+{
+    internal class Alpha
+    {
+        public List<int> _array;
+        public int _size;
+        
+        public Alpha(int size) 
+        {
+            _array = new List<int>(size);
+            _size = size;
+        }
+
+    }
+}
